eeeae7c1f1ac07e20ce03dd66b25cccf7ad39dde
wox <woxdark@mail.ru>
2020-04-19 21:03:59 +0300

    add task 3.6_PyTest to check

---
+++ 3.6_PyTest/test_items.py
@@ -0,0 +1,8 @@
+import time
+def test_items(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.implicitly_wait(10)
+    browser.get(link)
+    #time.sleep(15)#раскоментировать для визуальной проверки языка страницы
+    assert browser.find_element_by_class_name("btn-add-to-basket")\
+        , "Error: element btn-add-to-basket NOT FOUND!"
